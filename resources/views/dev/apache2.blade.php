dd4355ffbeec3c345ad388ce5b76f2f1d900665d
JimyDickson <jimytavares@hotmail.com>
2023-12-13 16:36:23 -0300

    enviando

--- resources/views/dev/apache2.blade.php
+++ resources/views/dev/apache2.blade.php
@@ -18,8 +18,8 @@
     <table class="table table-bordered" style="color:white; background-color:#252a37;">
       <thead>
         <tr>
-          <th scope="col" style="color:#c28ae5 !important; width:15%">SERVER COMMANDS</th>
-          <th scope="col" style="color:#c28ae5 !important;">comandos</th>
+          <th scope="col" style="color:#c28ae5 !important; width:15%">função</th>
+          <th scope="col" style="color:#c28ae5 !important;">Comandos Rotina Servidor Apache2</th>
         </tr>
       </thead>
       <tbody>
@@ -48,8 +48,8 @@
         <table class="table table-bordered" style="color:white; background-color:#252a37; margin-top:30px;">
       <thead>
         <tr>
-          <th scope="col" style="color:#c28ae5 !important; width:15%">CREATE SITE</th>
-          <th scope="col" style="color:#c28ae5 !important;">implementações no servidor</th>
+          <th scope="col" style="color:#c28ae5 !important; width:15%">função</th>
+          <th scope="col" style="color:#c28ae5 !important;">Implementações Web no servidor</th>
         </tr>
       </thead>
       <tbody>
@@ -60,6 +60,41 @@
         <tr>
           <th scope="row">Desativa um site depois de configurar um arquivo.conf na pasta sites-available</th>
           <td>sudo a2dissite nome_do_arquivo.conf</td>
+        </tr>
+      </tbody>
+    </table>
+        
+    <table class="table table-bordered" style="color:white; background-color:#252a37; margin-top:30px;">
+      <thead>
+        <tr>
+          <th scope="col" style="color:#c28ae5 !important; width:15%">função</th>
+          <th scope="col" style="color:#c28ae5 !important;">Gerar chave SSH - liberação servidor para github</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Gerar senha:</th>
+          <td>ssh-keygen -t ed25519 -C "seuEmail"</td>
+        </tr>
+        <tr>
+          <th scope="row">ele vai perguntar se vc deseja armazerar a chave ssh em "/root/.ssh/id_ed25519"</th>
+          <td>ENTER</td>
+        </tr>
+        <tr>
+          <th scope="row">Escolher uma senha</th>
+          <td>"digitar/criar uma senha"</td>
+        </tr>
+        <tr>
+          <th scope="row">comando para visualizar senha: </th>
+          <td>eval "$(ssh0agent -s)"</td>
+        </tr>
+          <tr>
+          <th scope="row">ENTER</th>
+          <td>ssh-add ~/.ssh/</td>
+        </tr>
+          <tr>
+          <th scope="row">Inserir a chave de acesso que fica dentro da pasta que foi criada a chave</th>
+          <td>ssh-add ~/.ssh/id_0000</td>
         </tr>
       </tbody>
     </table>
